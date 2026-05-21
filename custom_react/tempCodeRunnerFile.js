dd089bfbd8eb5909d3548fbe3d8ef5b9dee9d299
vikasyadav104 <vikasyadavskb247@gmail.com>
2026-05-21 10:17:47 +0530

    lect 4

---
+++ custom_react/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+function customRender(reactElement,container){
+    const domElement=document.createElement(reactElement.type);
+    domElement.innerHTML=reactElement.children;
+    domElement.setAttribute('href',reactElement.props.href);
+    domElement.setAttribute('target',reactElement.props.target);
+    container.appendChild(domElement);
+}
