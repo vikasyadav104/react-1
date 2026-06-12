6623482ee2f63c37e03e1a7518330708de075cce
vikasyadav104 <vikasyadavskb247@gmail.com>
2026-06-12 12:03:49 +0530

    redux

---
+++ 10_reduxtoolkit_todo/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  //read some documentation of redux toolkit
+  // create a store and then import configure store from redux toolkit and then create a slice and then import the slice in the store and then use the provider to wrap the app and then use the useSelector and useDispatch to get the state and dispatch the actions
+
+
+
+  return (
+    <>
+  <h1> Vikas this side</h1>
+    </>
+  )
+}
+
+export default App
